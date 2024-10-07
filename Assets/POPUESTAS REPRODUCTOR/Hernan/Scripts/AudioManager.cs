97a64f707bdfcea3102e1772eac4d667ce2f3450
epsilon598 <hernan_dav.gutierrez@uao.edu.co>
2024-10-07 13:11:09 -0500

    HernanAudioTest
    Agregue una escena donde puedes agarrar un objeto y hacerlo sonar con Fire1, al soltarlo deja de sonar, luego cerca de la pared te acercas y con fire2 suena y deja de sonar cuando te alejas o presionas fire2. El audio de la zona se reduce si el audio del objeto está sonando

---
+++ Assets/POPUESTAS REPRODUCTOR/Hernan/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource objectAudioSource;
+    public AudioSource zoneAudioSource;
+    public float reducedVolume = 0.2f;
+    private float originalZoneVolume;
+    public float fadeSpeed = 2f;
+    void Start()
+    {
+        if (zoneAudioSource != null)
+        {
+            originalZoneVolume = zoneAudioSource.volume;
+        }
+    }
+
+    void Update()
+    {
+        if (objectAudioSource != null && zoneAudioSource != null)
+        {
+            if (objectAudioSource.isPlaying)
+            {
+                zoneAudioSource.volume = Mathf.Lerp(zoneAudioSource.volume, reducedVolume, Time.deltaTime * fadeSpeed);
+            }
+            else
+            {
+                zoneAudioSource.volume = Mathf.Lerp(zoneAudioSource.volume, originalZoneVolume, Time.deltaTime * fadeSpeed);
+            }
+        }
+    }
+
+}
+
